2edbca236db326c70dd2f0c5bd600fc833011c96
Tyler Nguyen <tylern41653@gmail.com>
2023-06-03 16:21:32 -0400

    test

--- server/index.js
+++ server/index.js
@@ -33,6 +33,11 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
+// Test Message
+app.get('/', (req,res) => {
+  res.send('This site is working!');
+})
+
 /* FILE STORAGE */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
